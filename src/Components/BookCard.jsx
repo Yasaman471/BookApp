49bd28bd2a6c294b3c1211f5b5b72c40b0024b57
Yasaman Abedi <Yasaman471@gmail.com>
2025-11-23 19:38:19 +0330

    feat(BookCard): Implement full styling and add user like functionality
    Completely restyles the BookCard component for visual consistency and introduces the user-facing 'like' feature.
    Includes UI updates for the like button and comprehensive styling adjustments across the component.

--- src/Components/BookCard.jsx
+++ src/Components/BookCard.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+import { FaHeart } from "react-icons/fa";
+import styles from "./BookCard.module.css"
 
-function BookCard( {data : {id,author,image,language,pages,title,year } } ) {
+
+
+function BookCard( {data : {author,image,language,pages,title } } ) {
+    const[like,setLike] = useState(false)
+
+    const likeHandler = ()=>{
+        setLike(like=>!like)
+    }
   return (
-    <div>
+    <div className={styles.card}>
         <img src={image} alt={title} />
-        <div>
+        <div className={styles.info}>
            <h3>{title}</h3>  
            <p>{author}</p>
            <div>
@@ -11,7 +21,9 @@ function BookCard( {data : {id,author,image,language,pages,title,year } } ) {
             <span>{pages} pages</span>
            </div>
         </div>
-        <button>like</button>
+        <button onClick={likeHandler}>
+            <FaHeart  color={like ? "red" : "#e0e0e0"} fontSize="1.8rem"/>
+        </button>
     </div>
   )
 }
